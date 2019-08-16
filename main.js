4cfb1fb26efdeb72a2368191ef25120c13adcd57
brad-morrison <45197952+brad-morrison@users.noreply.github.com>
2019-08-16 22:25:34 +0100

    f

--- main.js
+++ main.js
@@ -9,19 +9,22 @@ function scrollAction()
 {
     console.log(window.pageYOffset);
     
-    if (pageYOffset > 140)
+    if (window.innerWidth > 1024)
     {
-        logo.style.width = "30%";
-        menu.style.borderBottom = "1px solid #e5e5e5";
-        menu.style.boxShadow = "0 2px 4px 0 rgba(0,0,0,.1)";
-        menuItems.style.borderBottom = "none";
-    }
-    else
-    {
-        logo.style.width = "60%";
-        menu.style.borderBottom = "none";
-        menuItems.style.borderBottom = "1px solid #e5e5e5";
-        menu.style.boxShadow = "none";
+        if (pageYOffset > 140)
+        {
+            logo.style.width = "30%";
+            menu.style.borderBottom = "1px solid #e5e5e5";
+            menu.style.boxShadow = "0 2px 4px 0 rgba(0,0,0,.1)";
+            menuItems.style.borderBottom = "none";
+        }
+        else
+        {
+            logo.style.width = "60%";
+            menu.style.borderBottom = "none";
+            menuItems.style.borderBottom = "1px solid #e5e5e5";
+            menu.style.boxShadow = "none";
+        }
     }
     
     /*if (pageYOffset > 1564)
